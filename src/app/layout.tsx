55295d576b5c4456110a3c1aad0da5669e0251e8
Bram Doppen <bramdoppen@gmail.com>
2023-06-19 12:34:30 +0200

    update gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,16 +23,16 @@ export default function RootLayout({
   return (
     <html lang="nl" className={baseFont.className}>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'GA_MEASUREMENT_ID');
+          gtag('config', '${process.env.GA_MEASUREMENT_ID}');
         `}
       </Script>
       <body>{children}</body>
